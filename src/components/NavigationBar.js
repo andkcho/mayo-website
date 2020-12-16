a62f1a7abe550ebf45f3e7e433b6f5f42eef92e1
andkcho <andrewc801@gmail.com>
2020-12-16 09:02:30 -0700

    first commit

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+.navbar {
+    background-color: #222
+}
+
+.navbar-brand, .navbar-nav .nav-link {
+    color: #bbb
+}
+`
